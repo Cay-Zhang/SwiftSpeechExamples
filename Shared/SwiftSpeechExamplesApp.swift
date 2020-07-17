492530873028d1330c46d560820971ee0833d961
Cay Zhang <zcy20020811@gmail.com>
2020-07-16 17:00:45 -0700

    Initial Commit

---
+++ Shared/SwiftSpeechExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpeechExamplesApp.swift
+//  Shared
+//
+//  Created by Cay Zhang on 2020/7/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpeechExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
